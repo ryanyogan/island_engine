3f9dbc37002bf24399403510d7aa50f0b66c66eb
Ryan Yogan <ryanyogan@gmail.com>
2017-04-22 10:14:36 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# IslandEngine
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
+
+  1. Add `island_engine` to your list of dependencies in `mix.exs`:
+
+    ```elixir
+    def deps do
+      [{:island_engine, "~> 0.1.0"}]
+    end
+    ```
+
+  2. Ensure `island_engine` is started before your application:
+
+    ```elixir
+    def application do
+      [applications: [:island_engine]]
+    end
+    ```
+
---
+++ lib/island_engine.ex
@@ -0,0 +1,20 @@
+defmodule IslandEngine do
+  use Application
+
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    # Define workers and child supervisors to be supervised
+    children = [
+      # Starts a worker by calling: IslandEngine.Worker.start_link(arg1, arg2, arg3)
+      # worker(IslandEngine.Worker, [arg1, arg2, arg3]),
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: IslandEngine.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
